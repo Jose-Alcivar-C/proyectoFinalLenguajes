df11cfb78df642d1d595c280f0374f7bced90d54
Allison Recalde <aldoreca@espol.edu.ec>
2023-01-15 16:54:16 -0500

    peliculas por año

--- main(Recalde).rb
+++ main(Recalde).rb
@@ -132,3 +132,72 @@ end
 puts "----------Distribucción de la duración de las peliculas de ciencia ficción ----------"
 #En pantalla se mostrará 10 distribuciones, en la grafica en R estarán todos los datos que contiene el array
 puts array_duracionCienciaFiccion.slice(0,10)
+
+class PeliculaPorAnio
+    
+    attr_accessor :anio
+    def initialize(anio)
+      @anio=anio
+    end
+    def guardar()
+        CSV.open('pruebaAnio.csv', 'a') do |csv| 
+        csv << %w[Anio Nombre_Pelicula Rating] 
+        
+        pelis=0; pag=1
+        while (pelis<5)
+            
+            link = "https://www1.pelisgratishd.io/estrenos/#{anio}/page/#{pag}/"
+            pagina = URI.open(link)
+            paginaParsed = Nokogiri::HTML(pagina.read)
+            itemsP= paginaParsed.css('.items')
+            prueb=itemsP.css('article')
+        
+            prueb.each do |item|
+                titulo= item.css('.data').inner_text
+                rating=item.css('div').css('.rating').inner_text
+                
+                duracion= item.css('.imdb + span + span').inner_text
+                csv << [@anio.to_s,titulo.to_s,rating.to_f]
+                
+            end
+            pelis+=1
+            pag+=1
+        end
+
+    end
+    
+    end
+    end
+    array_anio=["2018","2019","2020","2021","2022"]
+    array_anio.each do |anioP|
+        anio_peliculas = PeliculaPorAnio.new(anioP)
+        anio_peliculas.guardar()
+    
+    end
+    array_rating2018=[]
+    array_rating2019=[]
+    array_rating2020=[]
+    array_rating2021=[]
+    array_rating2022=[]
+  
+    archivoAño = File.read("pruebaAnio.csv")
+        lineas = archivoAño.split("\n")
+        i=0
+        lineas.each do |linea|
+        cadena=linea.split(",")
+        docrating=cadena[2]
+        rating=docrating.to_f
+        if(cadena[0]=="2018")
+            array_rating2018.push(rating)
+        elsif (cadena[0]=="2019")
+            array_rating2019.push(rating)
+            elsif (cadena[0]=="2020")
+            array_rating2020.push(rating)
+        elsif (cadena[0]=="2021")
+            array_rating2021.push(rating)
+        elsif (cadena[0]=="2022")
+            array_rating2022.push(rating)
+            
+        end
+      i+=1
+    end

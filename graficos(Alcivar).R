2fc63ec70d4a9311028d7329bfca3d41502b207c
Jose Alcivar <josealci@espol.edu.ec>
2023-01-19 20:18:25 -0500

    graficos version 02

--- graficos(Alcivar).R
+++ graficos(Alcivar).R
@@ -23,4 +23,30 @@ pregunta1 <- function(){
   
 }
 
+#----------
+
+pregunta2 <- function(){
+  
+  archivoGeneros = read.csv("GenerosPeliculasEcuador(Alcivar).csv")
+  
+  ordenado = archivoGeneros[order(archivoGeneros$CantidadPeliculas, decreasing = TRUE),]
+  
+  genero = ordenado$Genero[1:5]
+  
+  peliculas = ordenado$CantidadPeliculas[1:5]
+  
+  print(peliculas)
+  
+  diagrama = barplot(height = peliculas, names = genero, ylim = c(0, 50000),
+          col = c("#009ACD", "#00FF00", "#CDC673", "#AB82FF"), main = "Los cinco generos con mas peliculas en Ecuador",
+          xlab = "Generos", ylab = "Cantidad de peliculas")
+  
+  text(diagrama, peliculas+1500, labels = peliculas)
+  
+}
+
+#----------Llamar a las funciones que grafican cada pregunta----------
+
 pregunta1()
+
+pregunta2()

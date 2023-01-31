488a6e2d1f651e41605586c1b63099fb7c3d79b3
Jose Alcivar <josealci@espol.edu.ec>
2023-01-31 00:19:13 -0500

    arreglo

--- graficos(Alcivar).R
+++ graficos(Alcivar).R
@@ -10,17 +10,14 @@ pregunta1 <- function(){
   peliculas = archivoPaises$CantidadPeliculas
   
   colores = c("#009ACD", "#00FF00", "#CDC673", "#AB82FF")
+
   
-  plot(anio, peliculas, col= colores[paises], pch = 19, xlab = "Año", ylab = "Películas",
+  plot(x = anio, y = peliculas, col= colores[paises], pch = 19, xlab = "Año", ylab = "Películas",
        main = "Distribución de las películas en países de Centroamérica",
        sub = "Relación entre el año y la cantidad de películas en cada país.")
-  
-  axis(1, tck = 1, lty = 2, col = "gray")
-  
-  axis(2, tck = 1, lty = 2, col = "gray")
-  
-  legend(x = "topleft", inset = 0.05, legend = c("El Salvador", "Guatemala", "Honduras", "Panama"), 
-        fill = colores, title = "Paises")
+   
+  legend(x = "topleft", inset = c(0, 0),  cex = 0.7, legend = c("El Salvador", "Guatemala", "Honduras", "Panama"), 
+         lty = c(1,1,1,1), col = colores, lwd = 5, title = "Paises", horiz = FALSE)
   
 }
 
